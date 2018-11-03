77d136b5717e551718bfbedb9ca78560e93282f1
Nikhil Sharma <sharmanikhil99@gmail.com>
2018-11-03 03:44:19 -0700

    added toast

--- app/src/main/java/com/example/sameer/spotifyqueue/MainActivity.java
+++ app/src/main/java/com/example/sameer/spotifyqueue/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
@@ -30,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Started App", Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -38,10 +40,12 @@ public class MainActivity extends AppCompatActivity {
 
         // We will start writing our code here.
 
-        System.out.println("opened login");
+        //System.out.println("opened login");
+        Toast.makeText(this, "opened login", Toast.LENGTH_SHORT).show();
         AuthenticationRequest.Builder builder =
                 new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
-        System.out.println("BUILDER");
+        //System.out.println("BUILDER");
+        Toast.makeText(this, "BUILDER", Toast.LENGTH_SHORT).show();
 
         builder.setScopes(new String[]{"streaming"});
         AuthenticationRequest request = builder.build();
@@ -59,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
         spotify.getAlbum("2dIGnmEIy1WZIcZCFSj6i8", new Callback<Album>() {
             @Override
             public void success(Album album, Response response) {
+                Toast.makeText(MainActivity.this, album.name, Toast.LENGTH_SHORT).show();
                 Log.d("Album success", album.name);
             }
 
@@ -79,12 +84,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        Toast.makeText(this, "OnActivityResult", Toast.LENGTH_SHORT).show();
         super.onActivityResult(requestCode, resultCode, intent);
 
         // Check if result comes from the correct activity
         System.out.println("request code: " + requestCode);
+
         if (requestCode == REQUEST_CODE) {
             AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            Toast.makeText(this, response.getError(), Toast.LENGTH_SHORT).show();
             System.out.println("ERROR: ");
             System.out.println(response.getError());
 
@@ -93,6 +101,8 @@ public class MainActivity extends AppCompatActivity {
                 case TOKEN:
                     // Handle successful response
                     AccessToken = response.getAccessToken();
+                    Toast.makeText(this, "Toast", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, AccessToken, Toast.LENGTH_SHORT).show();
                     connected();
                     break;
 
